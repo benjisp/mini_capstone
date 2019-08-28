e6a3cfea4bf433a4129bce66b855748a15b1d62d
benji <benjisp018@gmail.com>
2019-08-28 12:59:39 -0400

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(
+  name: "Starburst",
+  price: 1,
+  image_url: "https://smhttp-ssl-60529.nexcesscdn.net/pub/media/catalog/product/cache/image/700x560/e9c3970ab036de70892d86c6d221abfe/s/t/starburst-fruit-chews.jpg",
+  description: "juicy"
+)
+product.save
+
+product = Product.new(
+  name: "Kit Kats",
+  price: 2,
+  image_url: "https://www.shelflife.ie/wp-content/uploads/2018/04/KitKat-350x350.jpg",
+  description: "crunchy"
+)
+product.save
+
+product = Product.new(
+  name: "Mints",
+  price: 3,
+  image_url: "https://media.candystore.com/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spearmint-starlight-mints-pile-470x470.jpg",
+  description: "fresh"
+)
+product.save
